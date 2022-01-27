06e532ca0e7cbe68b8bc07b9c6d130fd6164758a
Sergio Gragera <sergiogragera@bit2me.com>
2022-01-27 08:47:36 +0100

    Add renovate configuration

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    platform: 'github',
+    gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
+    username: 'renovate-bot',
+    onboarding: true,
+    onboardingConfig: {
+      extends: ['config:base']
+    },
+    repositories: [
+      'sergiogragera/renovatebot'
+    ]
+  }
+  
